05204dea033c2fb3b67d3ec801c466c6125937c7
michelleADMVC <michelle.admvc@gmai.com>
2020-04-24 17:06:55 -0400

    Nuevas configuraciones

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,6 @@
 module.exports={
-    secret : 'besthanudrink',
-    dbURI : 'mongodb://localhost/coctelapp'
+    dbUser: "parti_admin",
+    dbPass: "qMrMVKWmOl8a9ukh",
+    secret: 'besthanudrink',
+    dbURI: 'mongodb+srv://parti_admin:qMrMVKWmOl8a9ukh@partibocs-th0bh.mongodb.net/test?retryWrites=true&w=majority'
 }
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('./config')
 const uri = config.dbURI;
+console.log("Connecting to DB");
+
 mongoose.connect(uri,{
     useUnifiedTopology:true,
     useCreateIndex:true,
